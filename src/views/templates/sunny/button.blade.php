b971d0376e87b58b3cddd4a090a937292dc6f800
Rafael Pouzada <rafaelmpouzada@gmail.com>
2020-04-29 10:39:49 -0300

    Sunny Template - Alteracao da cor padrao do botao.

--- src/views/templates/sunny/button.blade.php
+++ src/views/templates/sunny/button.blade.php
@@ -6,8 +6,8 @@
             <tr class="large_only"><td class="w560" height="15" width="560"></td></tr>
             <tr>
                 <td class="w560" width="560">
-                    <div class="button-content" align="center" style=" background-color:{{ Config::get('beautymail.colors.button', '#15396B') }};">
-                        <a href="{{ $link }}" class="button">{{ $title }}</a>
+                    <div class="button-content" align="center">
+                        <a href="{{ $link }}" class="button" style=" background-color:{{ Config::get('beautymail.colors.button', '#15396B') }};">{{ $title }}</a>
                     </div>
                 </td>
             </tr>
